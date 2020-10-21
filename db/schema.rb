4ff684cff86f6050427b48b1e245dcc7a9883b99
andresochoa91 <andres_ochoa91@hotmail.com>
2020-10-21 16:10:48 -0700

    Fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_075601) do
+ActiveRecord::Schema.define(version: 2020_10_21_222602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_10_21_075601) do
   create_table "courses", force: :cascade do |t|
     t.string "course_name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "lesson_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -30,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_10_21_075601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
+  end
+
+  create_table "weeks", force: :cascade do |t|
+    t.string "week_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
